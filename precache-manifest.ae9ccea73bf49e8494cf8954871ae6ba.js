e9f46ebf414c151033b0a0fd576c5b0e0b516b1c
hanna-liisa.vilms <hanna-liisa.vilms@khk.ee>
2020-10-20 17:07:02 +0300

    Updates

---
+++ precache-manifest.ae9ccea73bf49e8494cf8954871ae6ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba886752f3bc5ef8a26929ab152992ea",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "74451bd78be77487a6be",
+    "url": "/react-quiz/static/css/main.0292f122.chunk.css"
+  },
+  {
+    "revision": "499d79df2c5aba5bfc87",
+    "url": "/react-quiz/static/js/2.5f05bdbc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-quiz/static/js/2.5f05bdbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74451bd78be77487a6be",
+    "url": "/react-quiz/static/js/main.c824df85.chunk.js"
+  },
+  {
+    "revision": "696ff2c064b27b314fc8",
+    "url": "/react-quiz/static/js/runtime-main.dd5a94dd.js"
+  }
+]);
